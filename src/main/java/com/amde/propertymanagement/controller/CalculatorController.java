8651e58d91d1d6d5d0f4c3f5123b552070ce288d
Amde <amdek2003@gmail.com>
2024-03-04 17:02:37 +0300

    Create test controller

--- src/main/java/com/amde/propertymanagement/controller/CalculatorController.java
+++ src/main/java/com/amde/propertymanagement/controller/CalculatorController.java
@@ -1,15 +1,30 @@
 package com.amde.propertymanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.amde.propertymanagement.dto.CalculatorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/calculator")
 public class CalculatorController {
 
     @GetMapping("/add")
-    public Double add(Double num1, Double num2){
+    public Double add(@RequestParam(name = "num1") Double num1, @RequestParam("num2") Double num2){
         return num1 + num2;
+    }
+
+    @GetMapping("/sub/{num1}/{num2}")
+    public Double sub(@PathVariable Double num1,@PathVariable Double num2){
+        return num1 - num2;
+    }
+
+    @PostMapping("/multiply")
+    public ResponseEntity<Double> multiply(@RequestBody CalculatorDto calculatorDto){
+        Double result = calculatorDto.getNum1() * calculatorDto.getNum2() *
+                calculatorDto.getNum3() * calculatorDto.getNum4();
+
+        ResponseEntity<Double> responseEntity = new ResponseEntity<Double>(result, HttpStatus.CREATED);
+        return responseEntity;
     }
 }
--- src/main/java/com/amde/propertymanagement/dto/CalculatorDto.java
+++ src/main/java/com/amde/propertymanagement/dto/CalculatorDto.java
@@ -1,5 +1,18 @@
 package com.amde.propertymanagement.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CalculatorDto {
+    @JsonProperty("num")
+    private Double num1;
+    private Double num2;
+
+    private Double num3;
+
+    private Double num4;
 
 }
